c23a35903828d6a311154a93304e6efd1ef5e1b4
Ivan Churkin <bahes.in@gmail.com>
2017-04-27 01:46:37 +0300

    nightly update

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const koa = require('koa');
+'use strict';
+
+const Koa = require('koa');
+const app = new Koa();
+
+const koaLogger = require('koa-logger');
+const koaSend = require('koa-send');
+
+const Router = require('koa-router');
+const router = new Router();
+
+app.use(koaLogger());
+
+app.use(async (ctx, next) => {
+  await koaSend(ctx, ctx.path, {
+    root: `${__dirname}/public`,
+  });
+
+  next();
+});
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Main page';
+});
+
+app.use(
+  router.routes()
+);
+
+app.listen(3000);
