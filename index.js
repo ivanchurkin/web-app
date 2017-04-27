6df80d572bcce410f0f7eb890e41b8fea7ba36c2
Ivan Churkin <ivan@rutorika.ru>
2017-04-27 12:04:19 +0300

    daily update

--- index.js
+++ index.js
@@ -4,27 +4,21 @@ const Koa = require('koa');
 const app = new Koa();
 
 const koaLogger = require('koa-logger');
-const koaSend = require('koa-send');
+
+// @todo
+// follow the package update
+const koaStatic = require('koa-static');
+
+app.use(koaLogger());
+app.use(koaStatic('public'));
 
 const Router = require('koa-router');
 const router = new Router();
 
-app.use(koaLogger());
-
-app.use(async (ctx, next) => {
-  await koaSend(ctx, ctx.path, {
-    root: `${__dirname}/public`,
-  });
-
-  next();
-});
-
-router.get('/', (ctx, next) => {
+router.get('/', (ctx) => {
   ctx.body = 'Main page';
 });
 
-app.use(
-  router.routes()
-);
+app.use(router.routes());
 
 app.listen(3000);
